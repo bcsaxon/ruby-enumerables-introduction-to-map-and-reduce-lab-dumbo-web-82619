5a155009c576055003982b83a96a0cadcf561475
Brett Saxon <brettcsaxon@gmail.com>
2019-08-20 17:30:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,3 +18,14 @@ end
 def reduce_to_total(source_array, starting_point = 0 )
   source_array.reduce(starting_point, :+)
 end 
+
+def reduce_to_all_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i] == false 
+      return false  
+    end 
+    i += 1 
+  end 
+  return true 
+end 
